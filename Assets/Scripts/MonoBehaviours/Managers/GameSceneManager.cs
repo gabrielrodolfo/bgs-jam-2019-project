e1c5bc810be7c8b406a3526479be7089d360da90
Gabriel <gabrielrodolfoctt@gmail.com>
2019-10-09 17:06:34 -0300

    - Added base scripts/managers.

---
+++ Assets/Scripts/MonoBehaviours/Managers/GameSceneManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : Manager
+{
+    private Scene currentLoadedScene;
+    
+    public event UnityAction<Scene, LoadSceneMode> OnSceneLoaded
+    {
+        add
+        {
+            SceneManager.sceneLoaded += value;
+        }
+        remove
+        {
+            SceneManager.sceneLoaded -= value;
+        }
+    }
+    public event UnityAction<Scene> OnSceneUnloaded
+    {
+        add
+        {
+            SceneManager.sceneUnloaded += value;
+        }
+        remove
+        {
+            SceneManager.sceneUnloaded -= value;
+        }
+    }
+
+    public AsyncOperation UnloadCurrentSceneAsync(){
+        if (currentLoadedScene != null) return SceneManager.UnloadSceneAsync(currentLoadedScene);
+        return null;
+    }
+
+    public AsyncOperation LoadSceneAsync(string name)
+    {
+        return SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/Managers/Toolbox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Toolbox : MonoBehaviour
+{
+    private static Toolbox instance;
+    private static Toolbox Instance {
+        get
+        {
+            if (instance == null) {
+                instance = FindObjectOfType<Toolbox>();
+            }
+
+            return instance;
+        }
+    }
+
+    private List<Manager> managers;
+
+    public static T GetManager<T>() where T : Manager
+    {
+        return instance.FindManager<T>();
+    }
+
+    private T FindManager<T>() where T : Manager
+    {
+        return managers.First(x => x is T) as T;
+    }
+
+    private void RegisterManagers()
+    {
+        managers = transform.GetComponentsInChildren<Manager>().ToList();
+    }
+
+    private void Start()
+    {
+        RegisterManagers();
+    }
+
+}
